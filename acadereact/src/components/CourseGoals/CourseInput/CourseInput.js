576e96e1686c94b9aabdf2fa41fdf5ac44339675
omar saade <omarsaadeh11@gmail.com>
2022-05-26 20:18:03 +0300

    react

--- acadereact/src/components/CourseGoals/CourseInput/CourseInput.js
+++ acadereact/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,46 @@
 import React, { useState } from 'react';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+
+// import styled from 'styled-components';
+import styles from './CourseInput.module.css';
+
+// // css
+// const FormControl = styled.div`
+// margin: 0.5rem 0;
+
+// & label {
+//   font-weight: bold;
+//   display: block;
+//   margin-bottom: 0.5rem;
+//   color:${props => (props.invalid ? 'red' : 'black')};
+// }
+
+
+// & input {
+// display: block;
+// width: 100%;
+// border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+// background: ${props => (props.invalid ? '#ffd7d7' : 'transparent')};
+// font: inherit;
+// line-height: 1.5rem;
+// padding: 0 0.25rem;
+// }
+
+
+// & input:focus {
+//   outline: none;
+//   background: #fad0ec;
+//   border-color: #8b005d;
+// }
+
+
+// `;
+//css
+
+
+
+
 
 const CourseInput = props => {
 
@@ -26,15 +65,9 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
-        <input
-          style={{
-            borderColor: !isValid ? 'red' : '#ccc',
-            background: !isValid ? 'salmon' : 'transparent'
-          }}
-          type="text"
-          onChange={goalInputChangeHandler} />
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
@@ -42,3 +75,38 @@ const CourseInput = props => {
 };
 
 export default CourseInput;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //iza 0 dawe ahmar //invalid= true //red
+  //iza >0 dawe abiad //invalid = false // black - transparent
+//   return (
+//     <form onSubmit={formSubmitHandler}>
+//       <FormControl invalid={!isValid}>
+//         <label>Course Goal</label>
+//         <input type="text" onChange={goalInputChangeHandler} />
+//       </FormControl>
+//       <Button type="submit">Add Goal</Button>
+//     </form>
+//   );
+// };
+
+// export default CourseInput;
