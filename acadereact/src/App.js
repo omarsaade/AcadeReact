17a0e9eccee4634e081987d6e4ac21c98c7252b1
omar saade <omarsaadeh11@gmail.com>
2022-05-27 22:33:14 +0300

    react

--- acadereact/src/App.js
+++ acadereact/src/App.js
@@ -5,16 +5,11 @@ import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
 import './App.css';
 
 const App = () => {
-
-
   const [courseGoals, setCourseGoals] = useState([
     { text: 'Do all exercises!', id: 'g1' },
     { text: 'Finish the course!', id: 'g2' }
   ]);
-  // console.log(courseGoals);// (2)[{… }, {… }]
 
-
-  //add new element
   const addGoalHandler = enteredText => {
     setCourseGoals(prevGoals => {
       const updatedGoals = [...prevGoals];
@@ -23,8 +18,6 @@ const App = () => {
     });
   };
 
-
-  //delete and rendering
   const deleteItemHandler = goalId => {
     setCourseGoals(prevGoals => {
       const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
@@ -32,13 +25,10 @@ const App = () => {
     });
   };
 
-
-  //if empty courseGoals
   let content = (
     <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
   );
 
-  //if not empty //override
   if (courseGoals.length > 0) {
     content = (
       <CourseGoalList items={courseGoals} onDeleteItem={deleteItemHandler} />
@@ -47,24 +37,11 @@ const App = () => {
 
   return (
     <div>
-      {/* form */}
       <section id="goal-form">
         <CourseInput onAddGoal={addGoalHandler} />
       </section>
-
-
-      {/* todo list */}
       <section id="goals">
-
-
         {content}
-        {/* {courseGoals.length > 0 && (
-          <CourseGoalList
-            items={courseGoals}
-            onDeleteItem={deleteItemHandler}
-          />
-        ) // <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
-        } */}
       </section>
     </div>
   );
--- acadereact/src/components/CourseGoals/CourseInput/CourseInput.js
+++ acadereact/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,60 +1,20 @@
 import React, { useState } from 'react';
 
 import Button from '../../UI/Button/Button';
-
-// import styled from 'styled-components';
 import styles from './CourseInput.module.css';
 
-// // css
-// const FormControl = styled.div`
-// margin: 0.5rem 0;
-
-// & label {
-//   font-weight: bold;
-//   display: block;
-//   margin-bottom: 0.5rem;
-//   color:${props => (props.invalid ? 'red' : 'black')};
-// }
-
-
-// & input {
-// display: block;
-// width: 100%;
-// border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
-// background: ${props => (props.invalid ? '#ffd7d7' : 'transparent')};
-// font: inherit;
-// line-height: 1.5rem;
-// padding: 0 0.25rem;
-// }
-
-
-// & input:focus {
-//   outline: none;
-//   background: #fad0ec;
-//   border-color: #8b005d;
-// }
-
-
-// `;
-//css
-
-
-
-
-
-const CourseInput = props => {
-
+const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
   const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
     if (event.target.value.trim().length > 0) {
       setIsValid(true);
     }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
     if (enteredValue.trim().length === 0) {
       setIsValid(false);
@@ -65,7 +25,9 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
+      <div
+        className={`${styles['form-control']} ${!isValid && styles.invalid}`}
+      >
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
@@ -75,38 +37,3 @@ const CourseInput = props => {
 };
 
 export default CourseInput;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  //iza 0 dawe ahmar //invalid= true //red
-  //iza >0 dawe abiad //invalid = false // black - transparent
-//   return (
-//     <form onSubmit={formSubmitHandler}>
-//       <FormControl invalid={!isValid}>
-//         <label>Course Goal</label>
-//         <input type="text" onChange={goalInputChangeHandler} />
-//       </FormControl>
-//       <Button type="submit">Add Goal</Button>
-//     </form>
-//   );
-// };
-
-// export default CourseInput;
